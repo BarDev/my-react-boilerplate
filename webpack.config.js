862d8ff991c1a9fa1f97dedfde01f88a1605a1db
BarDev <github@BarDev.com>
2017-07-29 17:52:47 -0600

    Cleanup configuration and move babel presets to .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,6 @@ module.exports = {
     //devtool: 'eval-source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
     devtool: '#source-map',
     entry: [
-        // must be first entry to properly set public path
-        //'./src/webpack-public-path',
-        //'react-hot-loader/patch',
-        // 'webpack-hot-middleware/client?reload=true',
         path.resolve(__dirname, 'src/index.js') // Defining path seems necessary for this to work consistently on Windows machines.
     ],
     //target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
@@ -21,9 +17,16 @@ module.exports = {
         publicPath: '/',
         filename: 'bundle.js'
     },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/, 
+                exclude: /node_modules/, 
+                loader: "babel-loader"
+            }
+        ]
+    },
     plugins: [
-        //new webpack.HotModuleReplacementPlugin(),
-        //new webpack.NoEmitOnErrorsPlugin(),
         new HtmlWebpackPlugin({     // Create HTML file that includes references to bundled CSS and JS.
             template: 'src/index.ejs',
             filename: "index.html"
@@ -33,30 +36,5 @@ module.exports = {
             // },
             // inject: true
         }),
-        // new webpack.LoaderOptionsPlugin({
-        //     minimize: false,
-        //     debug: true,
-        //     noInfo: true, // set to false to see a list of every file being bundled.
-        //     options: {
-        //         context: '/',
-        //     }
-        // })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader",
-                query: {
-                    presets: ["env", "react"]
-                }
-            },
-            // { test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file-loader' },
-            // { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
-            // { test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
-            // { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' },
-            // { test: /\.(jpe?g|png|gif)$/i, loader: 'file-loader?name=[name].[ext]' },
-            // { test: /\.ico$/, loader: 'file-loader?name=[name].[ext]' },
-            // {test: /(\.css|\.scss|\.sass)$/, loaders: ['style-loader', 'css-loader?sourceMap', 'postcss-loader', 'sass-loader?sourceMap']}
-        ]
-    }
+    ]
 };
